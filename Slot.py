81458b0260f6e73b9f9b380aac7230198bf18d20
SplashEmotions <Smertvam6@gmail.com>
2025-07-03 15:11:05 +0500

    add code

---
+++ Slot.py
@@ -0,0 +1,20 @@
+import random
+
+variations = [[1,1,1],[2,2,2],[3,3,3]]
+randomnums = []
+
+for _ in range(3):
+    randomnums.append(random.randint(1,3))
+    
+        
+if randomnums == variations[0]:
+    print(f'{randomnums}\nТы победил с малым каэфом')
+
+elif randomnums == variations[1]:
+    print(f'{randomnums}\nТы победил с средним каэфом')
+
+elif randomnums == variations[2]:
+    print(f'{randomnums}\nТы победил с высоким каэфом')
+
+else:
+    print(f'{randomnums}\nТы проиграл')
